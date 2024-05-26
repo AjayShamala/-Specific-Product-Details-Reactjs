0355deb9968b2c4095ba5c3f313a08c9977e3919
Shyamala Ajay Kumar <167419255+AjayShamala@users.noreply.github.com>
2024-05-26 18:27:55 +0530

    Update index.js

--- src/components/SimilarProductItem/index.js
+++ src/components/SimilarProductItem/index.js
@@ -4,17 +4,19 @@ const SimilarProductItem = props => {
   const {title, brand, imageUrl, rating, price} = productDetails
   return (
     <li className="container">
-      <img src={imageUrl} alt="product" className="image-1" />
-      <h1 className="main-heading">{title}</h1>
-      <p className="para">by {brand}</p>
-      <div className="row-container">
-        <p className="paras">RS {price}/-</p>
-        <p className="pars">{rating}</p>
-        <img
-          src="https://assets.ccbp.in/frontend/react-js/star-img.png"
-          alt="star"
-          className="image-2"
-        />
+      <img src={imageUrl} alt="product" className="thumbnail" />
+      <h1 className="title">{title}</h1>
+      <p className="brand">by {brand}</p>
+      <div className="product-details">
+        <p className="price">Rs {price}/-</p>
+        <div className="rating-container">
+          <p className="rating">{rating}</p>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/star-img.png"
+            alt="star"
+            className="star"
+          />
+        </div>
       </div>
     </li>
   )
